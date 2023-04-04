f76fee260377e4f0c88af408b0fca6783280dbcc
CherniSamar5 <126211412+CherniSamar5@users.noreply.github.com>
2023-04-04 16:34:03 +0100

    Add files via upload

---
+++ src/main/java/com/samar/produits/ProduitsApplication.java
@@ -0,0 +1,15 @@
+package com.samar.produits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class ProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/samar/produits/service/ProduitService.java
@@ -0,0 +1,11 @@
+package com.samar.produits.service;
+import java.util.List;
+import com.samar.produits.entities.Produit;
+public interface ProduitService {
+Produit saveProduit(Produit p);
+Produit updateProduit(Produit p);
+void deleteProduit(Produit p);
+ void deleteProduitById(Long id);
+Produit getProduit(Long id);
+List<Produit> getAllProduits();
+}
